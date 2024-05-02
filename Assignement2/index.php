0dad8b78564a662e36a234e56868fceea9fc640a
Emanuele Zof <zof.emanuele@gmail.com>
2024-05-02 21:07:58 +0200

    Scaffold Assignement 2

---
+++ Assignement2/index.php
@@ -0,0 +1,41 @@
+<?php
+    //include 'functions.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Assignement 2 - Esercizio 1</title>
+    <link href="css/main.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"/>
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Assignement 2</a>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Esercizio 1</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="esercizio2.php">Esercizio 2</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="flex-shrink-0">
+        <section class="container">
+            <h1 class="mt-5">Assignement 2 - Esercizio 1</h1>
+            <p class="lead">TODO</p>
+        </section>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.plot.ly/plotly-2.31.1.min.js" charset="utf-8"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -11,20 +11,14 @@
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">CyberSecurity</a>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/assignement1">Assignement 1</a>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </nav>
     </header>
     <main class="flex-shrink-0">
         <section class="container pt-3">
             <h1 class="mt-5">CyberSecurity</h1>
-            <a class="" href="/assignement1">Assignement 1</a>
+            <p><a class="" href="/assignement1">Assignement 1</a></p>
+            <p><a class="" href="/assignement2">Assignement 2</a></p>
         </section>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
