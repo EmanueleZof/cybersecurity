dfe75df11b5964c92bc9e3c6ec545572f10f963f
Emanuele Zof <zof.emanuele@gmail.com>
2024-05-02 22:45:39 +0200

    test bitwise operations

--- Assignement2/index.php
+++ Assignement2/index.php
@@ -1,5 +1,10 @@
 <?php
-    //include 'functions.php';
+    include 'spn.php';
+    
+    $input1 = '10100110'; //bindec('10100110'); //0b10100110;
+    $input2 = 0b10110110;
+    $key1 = '0010110010010101'; //bindec('0010110010010101'); //0b0010110010010101;
+    $key2 = 0b0010010010010101;
 ?>
 
 <!doctype html>
@@ -33,6 +38,21 @@
         <section class="container">
             <h1 class="mt-5">Assignement 2 - Esercizio 1</h1>
             <p class="lead">TODO</p>
+            <div class="monospace"><?php echo $input1 ?> XOR</div>
+            <div class="monospace">
+                <?php
+                $ar = str_split($key1, 8);
+                //$test = 0b00101100;
+                //echo printBinary($test)
+                echo $ar[1];
+                ?> 
+            =</div>
+            <div class="monospace">
+                <?php
+                $result = bindec($input1) ^ bindec($ar[1]);
+                echo printBinary($result);
+                ?>
+            </div>
         </section>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
---
+++ Assignement2/spn.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @see https://www.php.net/manual/en/function.decbin.php
+ */
+function printBinary($binary) {
+    return sprintf('%08b',  $binary);
+}
+?>
