7dbea7d152142694a16bbad094a1ff676312eb7f
Emanuele Zof <zof.emanuele@gmail.com>
2024-06-25 22:11:40 +0200

    Add comments

--- rocketlearn.local/src/libs/auth.php
+++ rocketlearn.local/src/libs/auth.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Verifica se un utente è già registrato nel database.
+ *
+ * @param mysqli $db Connessione al database.
+ * @param array $inputs Array associativo contenente 'userName' e 'userEmail'.
+ * @return bool Ritorna true se l'utente è già registrato, altrimenti false.
+ */
 function isAlreadyRegistered($db, $inputs) {
     $sql = 'SELECT user_ID FROM users WHERE user_name = "'.$inputs['userName'].'" OR user_email = "'.$inputs['userEmail'].'"';
     
@@ -7,10 +14,25 @@ function isAlreadyRegistered($db, $inputs) {
     return mysqli_num_rows($query) == 0 ? false : true;
 }
 
+/**
+ * Genera un codice di attivazione casuale.
+ *
+ * @return string Ritorna una stringa esadecimale di 32 caratteri generata casualmente.
+ */
 function generateActivationCode() {
     return bin2hex(random_bytes(16));
 }
 
+/**
+ * Registra un nuovo utente nel database.
+ *
+ * @param mysqli $db Connessione al database.
+ * @param array $inputs Array associativo contenente 'userName', 'userPassword' e 'userEmail'.
+ * @param string $activationCode Codice di attivazione generato per l'utente.
+ * @param string $gaSecret Segreto di Google Authenticator per l'utente.
+ * @return array Ritorna un array contenente un booleano per indicare il successo dell'operazione 
+ *               e una stringa con la data di scadenza dell'attivazione o un messaggio di errore.
+ */
 function registerUser($db, $inputs, $activationCode, $gaSecret) {
     $passwordHash = password_hash($inputs['userPassword'], PASSWORD_BCRYPT);
     $activationCode = password_hash($activationCode, PASSWORD_DEFAULT);
@@ -29,6 +51,14 @@ function registerUser($db, $inputs, $activationCode, $gaSecret) {
     return [false, ''];
 }
 
+/**
+ * Invia un'email di attivazione all'utente.
+ *
+ * @param string $email L'indirizzo email dell'utente.
+ * @param string $activationCode Il codice di attivazione generato per l'utente.
+ * @param string $activationExpiry La data di scadenza per l'attivazione.
+ * @return bool Ritorna true se l'email è stata inviata con successo, altrimenti false.
+ */
 function sendActivationEmail($email, $activationCode, $activationExpiry) {
     $activationLink = APP_URL.'/activate.php?userEmail='.$email.'&activationCode='.$activationCode;
 
@@ -42,6 +72,14 @@ function sendActivationEmail($email, $activationCode, $activationExpiry) {
     return $mail;
 }
 
+/**
+ * Elimina un utente dal database in base al suo ID e allo stato di attività.
+ *
+ * @param mysqli $db La connessione al database.
+ * @param int $id L'ID dell'utente da eliminare.
+ * @param int $active Stato di attività dell'utente (0 o 1).
+ * @return bool Ritorna true se l'utente è stato eliminato con successo, altrimenti false.
+ */
 function deleteUserByID($db, $id, $active = 0) {
     $sql = 'DELETE FROM users
             WHERE user_ID ='.$id.' and active='.$active;
@@ -54,6 +92,14 @@ function deleteUserByID($db, $id, $active = 0) {
     return false;
 }
 
+/**
+ * Trova un utente non verificato nel database in base all'email e al codice di attivazione.
+ *
+ * @param mysqli $db La connessione al database.
+ * @param string $email L'email dell'utente da trovare.
+ * @param string $activationCode Il codice di attivazione dell'utente.
+ * @return array|null Ritorna un array con le informazioni dell'utente se trovato e non scaduto, altrimenti null.
+ */
 function findUnverifiedUser($db, $email, $activationCode) {
     $sql = 'SELECT user_ID, activation_code, activation_expiry < now() as expired
             FROM users
@@ -74,6 +120,13 @@ function findUnverifiedUser($db, $email, $activationCode) {
     return null;
 }
 
+/**
+ * Attiva un utente nel database impostando il suo stato come attivo e registrando la data di attivazione.
+ *
+ * @param mysqli $db La connessione al database.
+ * @param int $userID L'ID dell'utente da attivare.
+ * @return bool Ritorna true se l'utente è stato attivato correttamente, altrimenti false.
+ */
 function activateUser($db, $userID) {
     $sql = 'UPDATE users
             SET active = 1,
@@ -88,6 +141,13 @@ function activateUser($db, $userID) {
     return false;
 }
 
+/**
+ * Trova un utente nel database utilizzando l'indirizzo email.
+ *
+ * @param mysqli $db La connessione al database.
+ * @param string $email L'indirizzo email dell'utente da trovare.
+ * @return array|null Ritorna un array associativo contenente i dati dell'utente se trovato, altrimenti null.
+ */
 function findUserByEmail($db, $email) {
     $sql = 'SELECT *
             FROM users
@@ -102,6 +162,12 @@ function findUserByEmail($db, $email) {
     return null;
 }
 
+/**
+ * Effettua l'accesso dell'utente nel sistema impostando l'ID utente nella sessione.
+ *
+ * @param mysqli $db La connessione al database.
+ * @param string $userEmail L'indirizzo email dell'utente che sta effettuando l'accesso.
+ */
 function signInUser($db, $userEmail) {
     $user = findUserByEmail($db, $userEmail);
     
@@ -111,6 +177,14 @@ function signInUser($db, $userEmail) {
     }
 }
 
+/**
+ * Verifica le credenziali di accesso dell'utente confrontando l'email e la password nel database.
+ *
+ * @param mysqli $db La connessione al database.
+ * @param string $userEmail L'indirizzo email dell'utente.
+ * @param string $userPassword La password inserita dall'utente.
+ * @return array|false Restituisce l'utente dal database se le credenziali sono corrette, altrimenti ritorna false.
+ */
 function verifyUser($db, $userEmail, $userPassword) {
     $user = findUserByEmail($db, $userEmail);
 
@@ -121,10 +195,21 @@ function verifyUser($db, $userEmail, $userPassword) {
     return false;
 }
 
+/**
+ * Verifica se un utente è attualmente loggato.
+ *
+ * @return bool Restituisce true se l'utente è loggato, altrimenti false.
+ */
 function isUserLoggedIn() {
     return isset($_SESSION[USER]['userID']);
 }
 
+/**
+ * Verifica se è necessario il login dell'utente per accedere alla pagina.
+ *
+ * @param array $page Le informazioni della pagina da controllare.
+ * @return void
+ */
 function requireLogin($page) {
     if ($page['restricted'] && $page['name'] != 'signin') {
         if (!isUserLoggedIn()) {
@@ -134,6 +219,12 @@ function requireLogin($page) {
     }
 }
 
+/**
+ * Esegue il logout dell'utente corrente invalidando la sessione attiva.
+ * Se l'utente è loggato, distrugge la sessione e reindirizza alla pagina di login.
+ *
+ * @return void
+ */
 function signOutUser() {
     if (isUserLoggedIn()) {
         unset($_SESSION[USER]);
@@ -142,6 +233,13 @@ function signOutUser() {
     }
 }
 
+/**
+ * Restituisce il nome utente dell'utente attualmente loggato.
+ * Se l'utente è loggato, restituisce il nome utente memorizzato nella sessione.
+ * Altrimenti, restituisce null.
+ *
+ * @return string|null Il nome utente dell'utente attualmente loggato, o null se non loggato.
+ */
 function curentUser() {
     if (isUserLoggedIn()) {
         return $_SESSION[USER]['username'];
